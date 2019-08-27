e5a24fa8d9b937ef81643c327bef345fdb8cc314
Jake Awe <aawe@jhctechnology.com>
2019-08-27 15:53:52 +0100

    created product component in app

--- src/ecommerce/views.py
+++ src/ecommerce/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.contrib.auth import authenticate, login
+from django.shortcuts import render, redirect
 from django.shortcuts import HttpResponse
-
+from .forms import ContactForm, LoginForm
 
 def home_page(request):
     context = {
@@ -17,12 +18,44 @@ def about_page(request):
     }
     return render(request, "about_page.html", context)
 
+def login_page(request):
+    form = LoginForm(request.POST or None)
+    context = { "form": form }
+    if form.is_valid():
+        print(form.cleaned_data)
+        username = form.cleaned_data.get("username")
+        password = form.cleaned_data.get("password")
+        user = authenticate(request, username=username, password=password)
+        print(user)
+        if user is not None:
+            # print(request.user.is_authenticated())
+            login(request, user)
+            # context['form'] = LoginForm()
+            return redirect("/login")
+        else:
+            print("Error")
+    return render(request, "auth/login.html", context)
+
+def register_page(request):
+    form = LoginForm(request.POST or None)
+    print("User logged in")
+    print(request.user.is_authenticated())
+    if form.is_valid():
+        print(form.cleaned_data)
+    return render(request, "auth/register.html", {})
+
+
 def contact_page(request):
+    contact_form = ContactForm(request.POST or None)
     context = {
         "title": "Contact!",
-        "content": "Welcome to the contact page"
-
+        "content": "Welcome to the contact page",
+        "form": contact_form
     }
+    if contact_form.is_valid():
+        print(contact_form.cleaned_data)
     if request.method == 'POST':
-        print(request.POST)
+        print(request.POST.get('fullname'))
+        print(request.POST.get('email'))
+        print(request.POST.get('content'))
     return render(request, "contact/view.html", context)
